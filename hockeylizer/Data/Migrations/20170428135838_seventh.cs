3dd549a2a6b8e848d727d3f8c834853a51666cb4
Lennart Hammarström <fizzkhen@gmail.com>
2017-04-28 16:51:39 +0200

    Updated database

---
+++ hockeylizer/Data/Migrations/20170428135838_seventh.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hockeylizer.Data.Migrations
+{
+    public partial class seventh : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
