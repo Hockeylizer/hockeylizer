c796838733b31b5ffcec9ae899db559c1e62d167
Daniel <danielzs@kth.se>
2017-04-28 17:52:07 +0200

    La till class som ska innehålla listan med analysresultat för en given video.

---
+++ hockeylizer/Models/AnalysisResultSet.cs
@@ -0,0 +1,20 @@
+﻿/* Hittades på av Daniel. Tanken är att det är en lista med
+ * alla individuella analysresultat för en given video.
+ */
+
+using System;
+using System.Runtime.InteropServices;
+
+namespace idk //TEMP Vad ska stå här?
+{
+    // The return value of AnalysisBridge.AnalyzeShot(...)
+    [StructLayout(LayoutKind.Sequential)]
+    public class AnalysisResultSet
+    {
+        //an AnalysisResult has: (int FrameNr, Point2i HitPoint, Point2d OffsetFromTarget, bool DidHitGoal)
+        public List<AnalysisResult> analysisResultList { get; set; }
+        public int numberOfHits { get; set; }
+        public int VideoId { get; set; }
+    }
+
+}
