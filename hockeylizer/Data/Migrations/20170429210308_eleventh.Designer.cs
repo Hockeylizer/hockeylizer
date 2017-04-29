74135fb3aa350f0363c58efbf050d9ac061e59ce
lhammarstrom <lhammarstroem@gmail.com>
2017-04-29 23:03:55 +0200

    Migrated database

--- hockeylizer/Data/Migrations/20170429210308_eleventh.Designer.cs
+++ hockeylizer/Data/Migrations/20170429210308_eleventh.Designer.cs
@@ -8,9 +8,10 @@ using hockeylizer.Data;
 namespace hockeylizer.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170429210308_eleventh")]
+    partial class eleventh
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.1")
