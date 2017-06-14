2556d97d8967ff9b51f316610f8a1c14b528a38e
Lennart Hammarström <lhammarstrom@users.noreply.github.com>
2017-06-14 11:53:05 +0200

    Updated database
    Updated database

---
+++ hockeylizer/Migrations/20170614095215_latest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace hockeylizer.Migrations
+{
+    public partial class latest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AimPoints",
+                columns: table => new
+                {
+                    AimPointId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    SessionId = table.Column<int>(nullable: false),
+                    XCoordinate = table.Column<int>(nullable: true),
+                    YCoordinate = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AimPoints", x => x.AimPointId);
+                    table.ForeignKey(
+                        name: "FK_AimPoints_Sessions_SessionId",
+                        column: x => x.SessionId,
+                        principalTable: "Sessions",
+                        principalColumn: "SessionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AimPoints_SessionId",
+                table: "AimPoints",
+                column: "SessionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AimPoints");
+        }
+    }
+}
