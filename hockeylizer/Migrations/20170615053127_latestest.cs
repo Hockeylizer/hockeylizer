b133754efd7dca12e6e08441fcca2b32d9a3a3fe
lhammarstroem <lhammarstroem@gmail.com>
2017-06-15 07:37:27 +0200

    Updated database to show changes

---
+++ hockeylizer/Migrations/20170615053127_latestest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hockeylizer.Migrations
+{
+    public partial class latestest : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AnalisysFailReason",
+                table: "Sessions",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ChopFailReason",
+                table: "Sessions",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnalisysFailReason",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "ChopFailReason",
+                table: "Sessions");
+        }
+    }
+}
