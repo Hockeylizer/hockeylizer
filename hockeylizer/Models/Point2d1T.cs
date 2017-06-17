7012b01178433b98a1d1e41b6b5bcc3297e60903
Daniel <danielzs@kth.se>
2017-06-17 18:10:14 +0200

    Added parameter validation to Point2d1T constructor.

--- hockeylizer/Models/Point2d1T.cs
+++ hockeylizer/Models/Point2d1T.cs
@@ -1,13 +1,13 @@
-﻿namespace hockeylizer.Models
+﻿using System;
+
+namespace hockeylizer.Models
 {
     /// <summary>
     /// A container for 2D points, together with an intented target int. 2d1T stands for 2 doubles 1 Target
     /// </summary>
     public class Point2d1T : Point2d {
-        public Point2d1T(double x, double y) : base(x, y)  {
-            this.target = -1;
-        }
         public Point2d1T(double x, double y, int target) : base(x, y) {
+            if (target < 1 || 5 < target) throw new ArgumentOutOfRangeException("Target must be in the range 1-5.");
             this.target = target;
         }
         public override string ToString() {
