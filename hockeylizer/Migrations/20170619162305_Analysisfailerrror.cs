57fca2ed4e284a7cff44994fc36b2da58b7935e0
lhammarstroem <lhammarstroem@gmail.com>
2017-06-19 18:23:44 +0200

    Added migration

---
+++ hockeylizer/Migrations/20170619162305_Analysisfailerrror.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hockeylizer.Migrations
+{
+    public partial class Analysisfailerrror : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AnalysisFailed",
+                table: "Sessions",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "ChopFailed",
+                table: "Sessions",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnalysisFailed",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "ChopFailed",
+                table: "Sessions");
+        }
+    }
+}
