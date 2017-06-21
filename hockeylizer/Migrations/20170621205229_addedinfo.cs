452613a3e4282c6a6d6a16442b56fc96daa18549
lhammarstroem <lhammarstroem@gmail.com>
2017-06-21 22:53:42 +0200

    Added migratiob

---
+++ hockeylizer/Migrations/20170621205229_addedinfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace hockeylizer.Migrations
+{
+    public partial class addedinfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Info",
+                table: "Targets",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Info",
+                table: "Targets");
+        }
+    }
+}
